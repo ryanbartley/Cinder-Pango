1f7d63a17bf22d590c73db34574f139ac60579c4
Ryan Bartley <ryanbartley@gmail.com>
2016-10-25 17:20:09 -0400

    Fixed the cmake system

--- proj/cmake/Cinder-PangoConfig.cmake
+++ proj/cmake/Cinder-PangoConfig.cmake
@@ -13,25 +13,38 @@ if( NOT TARGET Cinder-Pango )
 	string( TOLOWER "${CINDER_TARGET}" CINDER_TARGET_LOWER )
 	
   get_filename_component( PANGO_LIBS_PATH "${BLOCK_PATH}/lib/${CINDER_TARGET_LOWER}" ABSOLUTE )
-  get_filename_component( HARFBUZZ_LIBS_PATH "${BLOCK_PATH}/../Cinder-Harfbuzz/lib_p/${CINDER_TARGET_LOWER}" ABSOLUTE )
   set( Cinder-Pango_LIBRARIES 
-        ${PANGO_LIBS_PATH}/libpangocairo.a
-        ${PANGO_LIBS_PATH}/libpangooft2.a
+        ${PANGO_LIBS_PATH}/libpangocairo-1.0.a
+        ${PANGO_LIBS_PATH}/libpangoft2-1.0.a
         ${PANGO_LIBS_PATH}/libpango-1.0.a
-        ${HARFBUZZ_LIBS_PATH}/libharfbuzz-gobject.a
-        ${HARFBUZZ_LIBS_PATH}/libharfbuzz.a
+        ${PANGO_LIBS_PATH}/libharfbuzz-gobject.a
+        ${PANGO_LIBS_PATH}/libharfbuzz.a
+        ${PANGO_LIBS_PATH}/libcairo.a
+        ${PANGO_LIBS_PATH}/libpixman-1.a
+        ${PANGO_LIBS_PATH}/libpng16.a
         ${PANGO_LIBS_PATH}/libglib-2.0.a
         ${PANGO_LIBS_PATH}/libgio-2.0.a
         ${PANGO_LIBS_PATH}/libgobject-2.0.a
         ${PANGO_LIBS_PATH}/libgmodule-2.0.a
         ${PANGO_LIBS_PATH}/libgthread-2.0.a
+        ${PANGO_LIBS_PATH}/libasprintf.a
+        ${PANGO_LIBS_PATH}/libfontconfig.a
+        ${PANGO_LIBS_PATH}/libgettextpo.a
+        ${PANGO_LIBS_PATH}/libintl.a
+        ${PANGO_LIBS_PATH}/libffi.a
+        -lpcre
+        -liconv
+        -lexpat
+        "${CINDER_PATH}/${CINDER_LIB_DIRECTORY}/libcinder.a"
     )
   get_filename_component( PANGO_INCLUDE_PATH "${BLOCK_PATH}/include/${CINDER_TARGET_LOWER}" ABSOLUTE )
   get_filename_component( HARFBUZZ_INCLUDE_PATH "${BLOCK_PATH}/../Cinder-Harfbuzz/include_p/${CINDER_TARGET_LOWER}" ABSOLUTE )
   set( Cinder-Pango_INCLUDES 
         ${PANGO_INCLUDE_PATH}/pango-1.0 
         ${PANGO_INCLUDE_PATH}/glib-2.0
-        ${HARFBUZZ_INCLUDE_PATH}/harfbuzz  
+        ${PANGO_INCLUDE_PATH}/harfbuzz
+        ${PANGO_INCLUDE_PATH}/cairo  
+        ${PANGO_INCLUDE_PATH}
         ${CINDER_PATH}/include/freetype )
 
 endif()
